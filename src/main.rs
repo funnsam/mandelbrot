a1ac00ac7e6417681cf12b4bb8fa0f79c3751885
funnsam <funnsam8@gmail.com>
2024-06-11 06:35:05 +0000

    bettr

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 const FRAC: usize = 10;
-const CHARS: &[u8; 16] = b" .,ixqeawmIOLSA@";
+const CHARS: &str = "            ░▒▓█";
 
 fn iterate(c: (i16, i16)) -> usize {
     let mut z = (0_i16, 0_i16);
@@ -24,13 +24,20 @@ fn iterate(c: (i16, i16)) -> usize {
 fn main() {
     const SIZE: i16 = 64;
     const LOG: usize = SIZE.ilog2() as _;
+    const ZOOM: usize = 0;
+
+    const X_CENTER: f64 = -0.5;
+    const Y_CENTER: f64 = 0.0;
+
+    let x_off = (X_CENTER * 2.0_f64.powi(FRAC as _)).round() as i16;
+    let y_off = (Y_CENTER * 2.0_f64.powi(FRAC as _)).round() as i16;
 
     for y in 0..SIZE {
-        let y = (y as i16 - SIZE / 2) << (FRAC - LOG + 2);
+        let y = ((y as i16 - SIZE / 2) << (FRAC - LOG - ZOOM + 1)) + y_off;
 
         for x in 0..SIZE * 2 {
-            let x = ((x as i16 - SIZE) << (FRAC - LOG + 1)) - (1 << (FRAC - 1));
-            let c = CHARS[iterate((x, y))] as char;
+            let x = ((x as i16 - SIZE) << (FRAC - LOG - ZOOM)) + x_off;
+            let c = CHARS.chars().nth(iterate((x, y))).unwrap();
             print!("{c}");
         }
 
